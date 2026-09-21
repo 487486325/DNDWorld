54914f361542856e26abb957263637264d2a61ee
我也能学会吗 <487486325@qq.com>
2026-09-21 20:28:32 +0800

    更换项目

---
+++ src/main/java/com/example/dndworld/DndWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.dndworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndWorldApplication.class, args);
+    }
+
+}
